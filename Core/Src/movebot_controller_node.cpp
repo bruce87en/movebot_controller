e423db19c353b77c650c03cc3c098629494b8114
Bruce Wen <bruce87en@gmail.com>
2020-12-12 22:20:52 +0800

    subscribe cmd_vel

--- Core/Src/movebot_controller_node.cpp
+++ Core/Src/movebot_controller_node.cpp
@@ -10,6 +10,7 @@
 #include <tf/tf.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <geometry_msgs/Twist.h>
 
 #include "odometry.h"
 #include "movebot_controller_node.h"
@@ -26,12 +27,21 @@ std_msgs::Float32MultiArray odometry;
 ros::Publisher odometry_pub("odometry_data", &odometry);
 float odometry_data[7];
 
+void cmdMessageCb(const geometry_msgs::Twist& cmd_msg){
+	float vx = cmd_msg.linear.x;
+	float vy = cmd_msg.linear.y;
+	float rotate = cmd_msg.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", &cmdMessageCb);
 
 void MoveBotControllerNodeTask(void *argument)
 {
 	nh.initNode();
 
 	nh.advertise(odometry_pub);
+
+	nh.subscribe(cmd_sub);
 
 	odometry.data = odometry_data;
 	odometry.data_length = 7;
@@ -66,5 +76,6 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+// when DMA complete, STM32Hardware.rbuf may still have unread data
   nh.getHardware()->reset_rbuf();
 }
